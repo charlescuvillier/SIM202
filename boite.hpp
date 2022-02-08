b61540f2f1461cc177fcaf44f1b1b4d11cbd4533
charlescuvillier <81634112+charlescuvillier@users.noreply.github.com>
2022-02-08 09:30:04 +0100

    Update boite.hpp

--- boite.hpp
+++ boite.hpp
@@ -1,19 +1,28 @@
-#ifndef boite.hpp
-#define boite.hpp
+#ifndef boite_hpp
+#define boite_hpp
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <particule.hpp>
 using namespace std ;
 
 // classe boite
 
-class boite
+class Boite
 {   public:
-        int level;
-        vector<double> center;
-        vector<double> mass_center;
-        double mass;
-        Particule * particule;
-        Boite * boite_fille = NULL;
-        Boite * boite_soeur = NULL;
+        int niveau;
+        double taille;
+        vector<double> centre;
+        vector<double> centre_masse;
+        double masse;
+        Particule * particule; //
+        Boite * boite_fille = NULL;  //pointeur vers la premiere boite fille
+        Boite * boite_soeur = NULL;  //pointeur vers la premiere boite soeur
+
+        //constructeur de la boite
+        Boite(int niveau, vector<double>, double masse);
         }
+        //constructeur des boites filles
+        void constructionBoiteFille();
+
+#endif //boite.hpp
