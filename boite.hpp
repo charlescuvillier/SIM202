3cde5a8bd17a7ab1d995b8d770f2aad5c9584cd0
charlescuvillier <81634112+charlescuvillier@users.noreply.github.com>
2022-02-15 12:12:15 +0100

    Add files via upload

--- boite.hpp
+++ boite.hpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include <particule.hpp>
+#include "particule.hpp"
 using namespace std ;
 
 // classe boite
@@ -15,14 +15,28 @@ class Boite
         vector<double> centre;
         vector<double> centre_masse;
         double masse;
-        Particule * particule; //
-        Boite * boite_fille = NULL;  //pointeur vers la premiere boite fille
-        Boite * boite_soeur = NULL;  //pointeur vers la premiere boite soeur
-
+        Particule * particule; //vaut NULL si il n'y a pas exactement 1 particule dans la boite
+        Boite * boite_fille;  //pointeur vers la premiere boite fille
+        Boite * boite_soeur;  //pointeur vers la premiere boite soeur
+        vector<Particule*> PointeurParticuleDansBoite; //sert uniquement lors de la construction des boites
+        vector<Boite*> sousBoites; //sert uniquement lors de la construction des boites
         //constructeur de la boite
         Boite(int niveau, vector<double>, double masse);
-        }
+        //destructeur
+        ~Boite();
         //constructeur des boites filles
         void constructionBoiteFille();
+        //fonction pour savoir si il y a une paticule P
+        bool contient(const Particule& P) const;
+        //bilan des forces (Adrien)
+
+        //constructeurs
+        Boite(double taille = 1.0);
+        Boite(vector<double> centre, int niveau, double taille);
+        //generation des boites
+        void insertion(Particule * part);
+        void subdivision();
+        void generation(vector<Particule*> ListeParticule);
+};
 
 #endif //boite.hpp
