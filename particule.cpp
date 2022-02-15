685387c48a552a79ab28634d19b2b8f0de222ec0
LeoMicollet <81633896+LeoMicollet@users.noreply.github.com>
2022-02-15 09:24:44 +0100

    Add files via upload

--- particule.cpp
+++ particule.cpp
@@ -1,9 +1,10 @@
 #include "particule.hpp"
 
-Particule::Particule(vector<double>& pos, double m){
+Particule::Particule(vector<double>& pos, double m, vector<double> v){
     this->position.resize(2);
     this->position = pos;
-    this->vitesse.resize(2,0);
+    this->vitesse.resize(2);
+    this->vitesse = v;
     this->force.resize(2,0);
     this->masse = m;
 }
--- particule.hpp
+++ particule.hpp
@@ -17,18 +17,9 @@ class Particule{
         vector<double> force; //Force gravitationnelle exercée sur la particule
         double masse; //Masse de la particule
 
-        Particule(vector<double>& pos, double m); //constructeur à partir de la position de la particule
-        Particule(vector<double>& v, vector<double>& F); //constructeur à partir de la vitesse et de la force exercée sur la particule
-
+        Particule(vector<double>& pos, double m, vector<double> v); //constructeur à partir de la position de la particule
 };
 
-//=================================================================================
-//                        class ListeParticule
-//=================================================================================
 
-class ListeParticule{
-    public : 
-        vector<Particule> Liste;
-};
 
 #endif
