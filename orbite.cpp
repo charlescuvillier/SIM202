1fde1a28488da83ee903cd813fc50973c164d606
LeoMicollet <81633896+LeoMicollet@users.noreply.github.com>
2022-03-01 10:07:36 +0100

    Add files via upload

--- orbite.cpp
+++ orbite.cpp
@@ -15,20 +15,20 @@ Particule MAJ_forces(Particule& parti, float epsilon, float rayon, Boite* B)
 {
     Boite* A=B;
     Particule& part = parti;
-    if(A.fille !=NULL && dist(A.cdm-part.position)<rayon )  //surcharger l'opérateur - pour des vecteurs !
+    if(A->boite_fille !=NULL && dist(A->centre_masse, part.position)<rayon )  //ok
     {
-        Boite* temp = A.fille;
+        Boite* temp = A->boite_fille;
         A = temp;
         while(A!=NULL)
         {
-            part = MAJ_forces(part, epsilon, A);
-            Boite* tempsoeur= A.soeur;
+            part = MAJ_forces(part, epsilon, rayon, A);
+            Boite* tempsoeur= A->boite_soeur;
             A= tempsoeur;
         }
     }
     else{
-        float r=dist(part.position, A.cdm);
-        float norm = G*part1.mass*A.mass;
+        float r=dist(part.position, A->centre_masse);
+        float norm = G*part.masse*A->masse;
         if (r>epsilon)
         {
             norm=-norm/(pow(r,3)); //a la place on divise par r cube et on multiplie par le vecteur u2-u1 non normé
@@ -37,9 +37,9 @@ Particule MAJ_forces(Particule& parti, float epsilon, float rayon, Boite* B)
         {
             norm=-norm/(pow(epsilon,3));
         }
-        part.force[1]+=norm*(part.position[1]-A.cdm[1]);
-        part.force[2]+=norm*(part.position[2]-A.cdm[2]);
-        part.force[0]+=norm*(part.position[0]-A.cdm[0]);
+        part.force[1]+=norm*(part.position[1]-A->centre_masse[1]);
+        part.force[2]+=norm*(part.position[2]-A->centre_masse[2]);
+        part.force[0]+=norm*(part.position[0]-A->centre_masse[0]);
         
         //ici on rajoute la force du a la boite
 
@@ -49,32 +49,35 @@ Particule MAJ_forces(Particule& parti, float epsilon, float rayon, Boite* B)
 
 
 
-Liste_Particules initialisationVitesse(&Liste_Particules LP, float dt)
+vector<Particule*> initialisationVitesse(vector<Particule*> LP, float dt)
 {
-    for (part in LP)
+    for (Particule* part : LP)
     {
-        part.vitesse[1] += (dt*part.force[1])/(2*part.masse);
-        part.vitesse[2] += (dt*part.force[2])/(2*part.masse);
-        part.vitesse[0] += (dt*part.force[0])/(2*part.masse);
+        part->vitesse[1] += (dt*part->force[1])/(2*part->masse);
+        part->vitesse[2] += (dt*part->force[2])/(2*part->masse);
+        part->vitesse[0] += (dt*part->force[0])/(2*part->masse);
     }
+    return LP;
 }
 
-Liste_Particules MAJ_pos(&Liste.Particules LP, float dt)
+vector<Particule*> MAJ_pos(vector<Particule*> LP, float dt)
 {
-    for (part in LP)
+    for (Particule* part : LP)
     {
-        part.position[1] += dt*part.vitesse[1];
-        part.position[2] += dt*part.vitesse[2];
-        part.position[0] += dt*part.vitesse[0];
+        part->position[1] += dt*part->vitesse[1];
+        part->position[2] += dt*part->vitesse[2];
+        part->position[0] += dt*part->vitesse[0];
     }
+    return LP;
 }
 
-Liste_Particules MAJ_vitesse(&Liste_Particules LP, float dt)
+vector<Particule*> MAJ_vitesse(vector<Particule*> LP, float dt)
 {
-    for (part in LP)
+    for (Particule* part : LP)
     {
-        part.vitesse[1] += dt*part.force[1]/part.mass;
-        part.vitesse[2] += dt*part.force[2]/part.mass;
-        part.vitesse[0] += dt*part.force[0]/part.mass;
+        part->vitesse[1] += dt*part->force[1]/part->masse;
+        part->vitesse[2] += dt*part->force[2]/part->masse;
+        part->vitesse[0] += dt*part->force[0]/part->masse;
     }
+    return LP;
 }
--- particule.hpp
+++ particule.hpp
@@ -12,10 +12,10 @@ using namespace std;
 
 class Particule{
     public :
-        vector<double> position; //position instantanée de la particule
+        vector<double> position; //position instantanée de la particule        
+        double masse; //Masse de la particule
         vector<double> vitesse; //Vitesse instantanée de la particule
         vector<double> force; //Force gravitationnelle exercée sur la particule
-        double masse; //Masse de la particule
 
         Particule(vector<double>& pos, double m,vector<double>& v, vector<double>& F); //constructeur à partir des paramètres la position de la particule
 };
