ecd98e3c9ff459248e54cf7d078be38091408f10
LeoMicollet <81633896+LeoMicollet@users.noreply.github.com>
2022-03-20 22:26:29 +0100

    Update affichage.m
    Ajout des constantes

--- affichage.m
+++ affichage.m
@@ -1,18 +1,23 @@
-% On récupère les positions des particules en fonctioni du temps
-m=readmatrix('part.csv');
+% On récupère les les constantes utiles utilisées dans le code cpp
+C = readmatrix('const.csv');
+N = C(1);
+dt = C(2);
+Taille = C(3);
 
-% On sépare ensuite chaques colonnes pour séparer positions et temps
+% On récupère les positions des particules en fonctioni du temps
+m = readmatrix('part.csv');
+
+% On sépare ensuite chaques colonnes pour séparer les coordonnées
 x = m(:,2);
 y = m(:,3);
 z = m(:,4);
-t = m(:,5);
 
-for k = 1 : length(x)/3
+for k = 1 : length(x)/N
   % Création du nuage de points
   scatter3([x(k) y(k) z(k)], [x(k+1) y(k+1) z(k+1)], [x(k+2) y(k+2) z(k+2)]);
-  title( sprintf('T = %f',t(k*3)));
+  title( sprintf('T = %f',(k-1)*dt));
   %on représente la boîte racine
-  axis([-5 5 -5 5 -5 5])
+  axis([-Taille/2 Taille/2 -Taille/2 Taille/2 -Taille/2 Taille/2])
   hold off
   pause(1);
 end
