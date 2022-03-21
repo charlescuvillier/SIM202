f559ace1da959630d2c3c9c43a37daa1c817691f
charlescuvillier <81634112+charlescuvillier@users.noreply.github.com>
2022-03-21 18:05:11 +0100

    Update affichage.m

--- affichage.m
+++ affichage.m
@@ -1,4 +1,8 @@
 % On récupère les les constantes utiles utilisées dans le code cpp
+clear all;
+close all;
+clc;
+
 C = readmatrix('const.csv');
 N = C(1);
 dt = C(2);
@@ -17,7 +21,7 @@ for k = 1 :N: length(x)
   % Création du nuage de points
   scatter3(x(k:k+N-1), y(k:k+N-1), z(k:k+N-1));
   axis([-Taille Taille -Taille Taille -Taille Taille])
-  title( sprintf('T = %f',(k-1)*dt));
+  title( sprintf('T = %f',((k-1)/N)*dt));
   xlabel('x');
   ylabel('y');
   zlabel('z');
